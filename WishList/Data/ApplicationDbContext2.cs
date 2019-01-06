38247dd9cde7d6efd78f345f29c51b66363f881c
Rodney Rollison <rodney.rollison@gmail.com>
2019-01-06 08:13:11 -0600

    update file name

--- WishList/Data/ApplicationDbContext2.cs
+++ WishList/Data/ApplicationDbContext2.cs
@@ -11,7 +11,7 @@ namespace WishList.Data
     {
         public ApplicationDbContext(DbContextOptions options):base(options)
         {            
-
+             
         }
 
         public DbSet<Item> Items;
