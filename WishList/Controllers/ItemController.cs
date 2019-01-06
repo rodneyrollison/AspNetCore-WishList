aa683c35d193b9c52ab51572a3197680aba6ca22
Rodney Rollison <rodney.rollison@gmail.com>
2019-01-06 08:46:34 -0600

    all modules complete

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -12,9 +12,9 @@ namespace WishList.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public ItemController(ApplicationDbContext _context)
+        public ItemController(ApplicationDbContext context)
         {
-
+            _context = context;
         }
 
         public IActionResult Index()
